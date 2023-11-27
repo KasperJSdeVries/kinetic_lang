fc374fc7cc61fdad40380f5714c5c824cad37d16
Kasper de Vries <kasper.j.s.de.vries@gmail.com>
2023-11-27 17:06:55 +0100

    Lexer: Add Token type
    The token currently has a lot of data on it, might remove some in the
    future, Token.lexeme seems especially useless.
    
    The TokenType enum is currently not filled out yet but that will happen
    as more parts of the language are added.

---
+++ include/lexer/token.h
@@ -0,0 +1,95 @@
+#ifndef _KINETIC_LANG_TOKEN_H
+#define _KINETIC_LANG_TOKEN_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define ENUMERATE_TOKEN_TYPES                                                  \
+	__ENUMERATE_TOKEN_TYPE(INVALID)                                            \
+	__ENUMERATE_TOKEN_TYPE(EOF)
+
+typedef enum {
+#define __ENUMERATE_TOKEN_TYPE(x) TOKEN_##x,
+	ENUMERATE_TOKEN_TYPES
+#undef __ENUMERATE_TOKEN_TYPE
+} TokenType;
+
+static char *token_type_as_string(TokenType type) {
+	switch (type) {
+#define __ENUMERATE_TOKEN_TYPE(x)                                              \
+	case TOKEN_##x:                                                            \
+		return #x;
+		ENUMERATE_TOKEN_TYPES
+#undef __ENUMERATE_TOKEN_TYPE
+	}
+}
+
+typedef struct {
+	size_t line;
+	size_t column;
+} Position;
+
+Position position_create(size_t line, size_t column) {
+	return (Position){
+	    .column = column,
+	    .line = line,
+	};
+}
+
+typedef struct {
+	const char *ptr;
+	size_t length;
+} Lexeme;
+
+Lexeme lexeme_create(const char *input, size_t start_offset, size_t length) {
+	return (Lexeme){
+	    .ptr = input + start_offset,
+	    .length = length,
+	};
+}
+
+typedef struct {
+	TokenType type;
+	Lexeme lexeme;
+	Position start;
+	Position end;
+	union {
+		union {
+			char character;
+			char *string;
+
+			uint8_t u8;
+			uint16_t u16;
+			uint32_t u32;
+			uint64_t u64;
+
+			int8_t i8;
+			int16_t i16;
+			int32_t i32;
+			int64_t i64;
+
+			bool b8;
+
+			float f32;
+			double f64;
+		} literal;
+
+		// NOTE: Can add things like symbol table entry data
+		char *identifier;
+	} data;
+} Token;
+
+Token token_create(TokenType type, Lexeme lexeme, Position start,
+                   Position end) {
+	return (Token){
+	    .type = type,
+	    .lexeme = lexeme,
+	    .start = start,
+	    .end = end,
+	};
+}
+
+#undef ENUMERATE_TOKEN_TYPES
+
+#endif // _KINETIC_LANG_TOKEN_H
--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
+#include <lexer/token.h>
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello world!\n");
-  return 0;
+	Token t = {0};
+	printf("Token.type: %lu\n", sizeof(t.type));
+	printf("Token.lexeme: %lu\n", sizeof(t.lexeme));
+	printf("Token.start: %lu\n", sizeof(t.start));
+	printf("Token.end: %lu\n", sizeof(t.end));
+	printf("Token.data: %lu\n", sizeof(t.data));
+	return 0;
 }
