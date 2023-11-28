b3bc9fdc3335defde5c9581c3a4ba0190c3d3558
Kasper de Vries <kasper.j.s.de.vries@gmail.com>
2023-11-28 16:50:01 +0100

    Lexer: Add helper functions

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,4 +1,5 @@
 #include <lexer/lexer.h>
+#include <lexer/token.h>
 
 // std
 #include <stdlib.h>
@@ -7,10 +8,20 @@ struct Lexer_t {
 	const char *input;
 
 	size_t current_index;
+	size_t current_line;
 	size_t line_start;
 
 	Token *tokens;
+	size_t tokens_length;
+	size_t tokens_capacity;
 };
+
+void lexer_add_token(Lexer *lexer, Token token);
+void lexer_add_single_char_token(Lexer *lexer, TokenType type);
+char lexer_current(const Lexer *lexer);
+char lexer_peek(const Lexer *lexer, int distance);
+bool lexer_at_end(const Lexer *lexer);
+void lexer_skip_whitespace(Lexer *lexer);
 
 Lexer *lexer_create(const char *input) {
 	if (input == NULL) {
@@ -19,17 +30,86 @@ Lexer *lexer_create(const char *input) {
 
 	Lexer *lexer = malloc(sizeof(struct Lexer_t));
 
-	// TODO: Implement this
+	lexer->current_index = 0;
+	lexer->line_start = 0;
+	lexer->input = input;
+
+	lexer->tokens_capacity = 8;
+	lexer->tokens = calloc(lexer->tokens_capacity, sizeof(Token));
+	lexer->tokens_length = 0;
 
 	return lexer;
 }
 
 Token *lexer_lex(Lexer *lexer, size_t *out_token_count) {
-	// TODO: Implement this
-	return NULL;
+	while (!lexer_at_end(lexer)) {
+		lexer_skip_whitespace(lexer);
+	}
+
+	lexer_add_single_char_token(lexer, TOKEN_EOF);
+	*out_token_count = lexer->tokens_length;
+
+	return lexer->tokens;
 }
 
 void lexer_free(Lexer *lexer) {
 	// TODO: Expand as necessary
+	free(lexer->tokens);
 	free(lexer);
+}
+
+void lexer_add_token(Lexer *lexer, Token token) {
+	if (lexer->tokens_length >= lexer->tokens_capacity) {
+		lexer->tokens_capacity *= 2;
+		lexer->tokens =
+		    reallocarray(lexer->tokens, lexer->tokens_capacity, sizeof(Token));
+	}
+
+	lexer->tokens[lexer->tokens_length++] = token;
+}
+
+void lexer_add_single_char_token(Lexer *lexer, TokenType type) {
+	lexer_add_token(
+	    lexer,
+	    token_create(
+	        type, lexeme_create(lexer->input, lexer->current_index, 1),
+	        position_create(
+	            lexer->current_line, lexer->current_index - lexer->line_start
+	        ),
+	        position_create(
+	            lexer->current_line, lexer->current_index - lexer->line_start
+	        )
+	    )
+	);
+}
+
+char lexer_current(const Lexer *lexer) {
+	return lexer->input[lexer->current_index];
+}
+
+char lexer_peek(const Lexer *lexer, int distance) {
+	return lexer->input[lexer->current_index + distance];
+}
+
+bool lexer_at_end(const Lexer *lexer) {
+	return lexer_current(lexer) == '\0';
+}
+
+void lexer_skip_whitespace(Lexer *lexer) {
+	for (;;) {
+		switch (lexer_peek(lexer, 1)) {
+			case ' ':
+			case '\r':
+			case '\t':
+				lexer->current_index++;
+				break;
+			case '\n':
+				lexer->current_index++;
+				lexer->line_start = lexer->current_index;
+				lexer->current_line++;
+				break;
+			default:
+				return;
+		}
+	}
 }
--- test/lexer/lexer_test.cpp
+++ test/lexer/lexer_test.cpp
@@ -1,15 +1,36 @@
+#include <cstddef>
 #include <gtest/gtest.h>
 
 extern "C" {
 #include <lexer/lexer.h>
+#include <lexer/token.h>
 }
 
 TEST(lexer_test, create_lexer_should_return_valid_pointer) {
+	// Act
 	Lexer *lexer = lexer_create("");
+
+	// Assert
 	ASSERT_NE(lexer, nullptr);
 }
 
 TEST(lexer_test, create_lexer_with_null_should_return_null) {
+	// Act
 	Lexer *lexer = lexer_create(nullptr);
+
+	// Assert
 	ASSERT_EQ(lexer, nullptr);
+}
+
+TEST(lexer_test, lexing_empty_input_should_return_array_containing_TOKEN_EOF) {
+	// Arrange
+	Lexer *lexer = lexer_create("");
+	size_t token_count;
+
+	// Act
+	Token *tokens = lexer_lex(lexer, &token_count);
+
+	// Assert
+	ASSERT_EQ(token_count, 1);
+	ASSERT_EQ(tokens[0].type, TOKEN_EOF);
 }
