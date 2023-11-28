a1f67e58e2bfb69ac10b5e5f4658df6a11668400
Kasper de Vries <kasper.j.s.de.vries@gmail.com>
2023-11-28 16:47:41 +0100

    Assertions: Add assertions

---
+++ include/assert/assert.h
@@ -0,0 +1,27 @@
+#ifndef _KINETIC_LANG_ASSERT_H
+#define _KINETIC_LANG_ASSERT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+__attribute__((noreturn)) static void assertion_failed(
+    const char *assertion,
+    const char *file,
+    int line,
+    const char *function
+) {
+	printf("%s at %s:%d in %s\n", assertion, file, line, function);
+	exit(EXIT_FAILURE);
+}
+
+#define VERIFY(expr)                                                           \
+	((!(expr))                                                                 \
+	     ? assertion_failed(#expr, __FILE__, __LINE__, __PRETTY_FUNCTION__)    \
+	     : (void)0)
+
+#define ASSERT_NOT_REACHED() VERIFY(false)
+
+#define TODO()                                                                 \
+	assertion_failed("TODO", __FILE__, __LINE__, __PRETTY_FUNCTION__);
+
+#endif // _KINETIC_LANG_ASSERT_H
