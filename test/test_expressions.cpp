4c13ab0c5cfbbeae3150570a3158cf37660c2052
Kasper de Vries <kasper.j.s.de.vries@gmail.com>
2023-12-17 21:29:45 +0100

    Move lexer test to its own file

--- test/test_expressions.cpp
+++ test/test_expressions.cpp
@@ -2,34 +2,6 @@
 
 extern "C" {
 #include "expression.h"
-#include "lexer.h"
-#include "token.h"
-}
-
-TEST(expressions, test_lexer) {
-	struct lexer lexer = lexer_new("1 + 2 * 3 !");
-	struct token token;
-
-	token = lexer_next(&lexer);
-	ASSERT_EQ(token.type, TOKEN_ATOM);
-	ASSERT_EQ(token.lexeme, '1');
-	token = lexer_next(&lexer);
-	ASSERT_EQ(token.type, TOKEN_OP);
-	ASSERT_EQ(token.lexeme, '+');
-	token = lexer_next(&lexer);
-	ASSERT_EQ(token.type, TOKEN_ATOM);
-	ASSERT_EQ(token.lexeme, '2');
-	token = lexer_next(&lexer);
-	ASSERT_EQ(token.type, TOKEN_OP);
-	ASSERT_EQ(token.lexeme, '*');
-	token = lexer_next(&lexer);
-	ASSERT_EQ(token.type, TOKEN_ATOM);
-	ASSERT_EQ(token.lexeme, '3');
-	token = lexer_next(&lexer);
-	ASSERT_EQ(token.type, TOKEN_OP);
-	ASSERT_EQ(token.lexeme, '!');
-	token = lexer_next(&lexer);
-	ASSERT_EQ(token.type, TOKEN_EOF);
 }
 
 TEST(expressions, test_single_char) {
